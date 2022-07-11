df1dd5ae34a65ecc3f9ff42cc11d4b87d4bd9230
officialShubhamSharma <108714184+officialShubhamSharma@users.noreply.github.com>
2022-07-11 17:04:45 +0530

    Update SocialbackendApplication.java

--- backend/src/main/java/com/olacapstone/socialbackend/SocialbackendApplication.java
+++ backend/src/main/java/com/olacapstone/socialbackend/SocialbackendApplication.java
@@ -2,18 +2,12 @@ package com.olacapstone.socialbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SocialbackendApplication extends SpringBootServletInitializer{
+public class SocialbackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SocialbackendApplication.class, args);
 	}
-	@Override
-        protected SpringApplicationBuilder configure(SpringApplicationBuilder builder){
-        return builder.sources(SocialbackendApplication.class);
-        }
 
 }
